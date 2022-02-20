aae6e0f639c47a8b761e20669b33ce2b6fc4f5bc
Joe Jamin <gksekausgksmsskawk@gmail.com>
2022-02-20 17:29:42 +0900

    0220

--- tetris.cpp
+++ tetris.cpp
@@ -2,8 +2,32 @@
 #include <windows.h>
 #include <conio.h>
 
+/* **********************일지 및 작동법***********************
+
+1. w(회전) 후 같은블록이 나오면 코드가 멈추는 현상이 발생 
+=> 클래스의 shape자체를 수정하지 말고 temp에 넣어놨다가 map에 복사 (2월 20일 수정완료후 해결됨)
+=> 애초에 복사생성자로 블럭을 수많이 만들어놓고 map에 놓으면 해결될일이었음, 하나의 객체로 복사하고 붙여넣으려다보니까 문제가 발생함
+
+2. 이동키 너무 빨리누르면 버그 발생
+
+3. 왼쪽벽 넘어가면 오른쪽벽에서 나오는 버그발생 (임시 블럭 벽 만들면 해결될듯)
+
+
+
+추가할 사항 :
+  1. Game class 만들어서 게임 시작, 종료, 목숨 변수 추가
+  2. 랜덤함수 구현
+  3. 블럭 내려오는 속도 변화 구현
+
+작동법 : 
+  a : 왼쪽이동
+  d : 오른쪽이동
+  w : 회전
+*/
+
 class Block {
   friend class Map;
+  friend class Block_rotate;
   protected :
     int block_i = 9;
     int block_j = 9;
@@ -20,6 +44,7 @@ class Block {
       {0,0,0,0,0,0,0,0,0}
     };
   public :
+    void copy_block(Block & block_copy);
     void show_block(){
       for(int i=0; i<9; i++){
         for (int j=0; j<9; j++){
@@ -30,6 +55,26 @@ class Block {
       }
     }
     void rotate_block(); //회전함수
+};
+
+//버그 해결 전략1 => temp 만들어서 복사하고 그거를 ㅇㅋ?
+void Block::copy_block(Block & block_copy){
+  for(int i=0; i<block_i; i++){
+    for(int j=0; j<block_j; j++){
+      shape[i][j] = block_copy.shape[i][j];
+    }
+  }
+}
+
+class Block_rotate : public Block {
+  public:
+    Block_rotate(Block & block){
+      for(int i=0; i<block_i; i++){
+        for(int j=0; j<block_j; j++){
+          shape[i][j] = block.shape[i][j];
+        }
+      }
+    }
 };
 
 class Block_1 : public Block { // ㅡ 모양 블럭 ㅋㅋ
@@ -103,7 +148,7 @@ class Block_7 : public Block {
 };
 
 class Map {
-  bool block_check = true;
+  bool block_check;
   const int map_i = 15;
   const int map_j = 15;
   int map[15][15] = //동적할당으로 2차원배열 받는것 아직구현 안함
@@ -186,9 +231,9 @@ void Map::block_setter(Block & block, int x, int y){ //y에 3이들어가면 오
   
   for (int i=0; i<block.block_i; i++){
     for (int j=0; j<block.block_j; j++){
-      if(((x+i >= map_i) || (y+j >= map_j) )&& block.shape[i][j]==1){is_block_on = false;}
-      if((x+i > map_i) || (y+j > map_j)){continue;}
-      if(map[x+i][y+j] == 1 && block.shape[i][j]) {std::cout<<"??"<<std::endl; is_block_on = false;} // 애초에 맵에 놓으려는 곳에 블럭이 있을경우 못놓는다
+      if(((x+i >= map_i) || (y+j >= map_j) )&& block.shape[i][j]==1){is_block_on = false; break;}
+      if((x+i >= map_i) || (y+j >= map_j)){continue;}
+      if(map[x+i][y+j] == 1 && block.shape[i][j]) {is_block_on = false; break;} // 애초에 맵에 놓으려는 곳에 블럭이 있을경우 못놓는다
     }
   }
   if(is_block_on){
@@ -225,7 +270,7 @@ void Map::clear_block(){
 }
 
 void Map::show_map(){
-  std::cout<<"**********"<<std::endl;
+  std::cout<<"***************"<<std::endl;
   for (int i=0; i<map_i; i++){
     for (int j=0; j<map_j; j++){
       if(map[i][j] == 1){std::cout<<"o";} //블럭
@@ -233,7 +278,7 @@ void Map::show_map(){
     }
     std::cout<<std::endl;
   }
-  std::cout<<"**********"<<std::endl;
+  std::cout<<"***************"<<std::endl;
 }
 
 void Block::rotate_block(){//종결
@@ -254,19 +299,14 @@ void Block::rotate_block(){//종결
 class Game{
   Block * block_list = new Block[7];
   int * random_list = new int[10];
-
-
-
   public:
     Game(){
-
     }
-
-    int get_random_list(int i){
+    int get_random_list(int i){ //아직 구현 안함
       return random_list[i];
     }
 
-    Block get_block_list(int i){
+    Block get_block_list(int i){ //아직 구현안함
       return block_list[i];
     }
 
@@ -274,8 +314,13 @@ class Game{
       int q = -3; //떨어지기 시작하는 위치
       int r = 2;
       map.block_setter(block,q,r);
-      char c = 0;
-      map.set_block_check(true);
+ 
+      char c;
+      //std::cout<<"bug test1"<<std::endl;
+      map.set_block_check(true); //버그 원인 <= 이 코드가 실행되려하면 코드가 멈추는 현상 발생
+      //std::cout<<"bug test2"<<std::endl;
+
+      Block_rotate temp(block); //원본 저장
       while(c!=13 && map.get_block_check()) {
         if(_kbhit()){
           c = _getch();
@@ -287,31 +332,35 @@ class Game{
             map.block_remove(block,q,r);
             block.rotate_block();
             map.block_setter(block,q,r);
+
           }
         }
-        //입력값 input
+        
         map.block_remove(block,q,r);
         map.block_setter(block,q+1,r);
         q++;
-        Sleep(800);
+        Sleep(500);
         if(map.get_block_check()){map.show_map();}
         
       }
       map.block_setter(block,q-1,r);
+      map.show_map();
+      block.copy_block(temp);
+      //block = temp; //다시 복사
     }
 };
 
 
 int main(){
 
-  int random_list[10];
+  int random_list[10]; //임시로 랜덤리스트 설정함
   random_list[0] = 0;
-  random_list[1] = 0;
-  random_list[2] = 0;
-  random_list[3] = 3;
-  random_list[4] = 3;
-  random_list[5] = 4;
-  random_list[6] = 1;
+  random_list[1] = 4;
+  random_list[2] = 7;
+  random_list[3] = 0;
+  random_list[4] = 4;
+  random_list[5] = 7;
+  random_list[6] = 0;
   random_list[7] = 2;
   random_list[8] = 7;
   random_list[9] = 0;
@@ -324,7 +373,7 @@ int main(){
   Block_7 block_7;
 
   Block block_list[7];
-  block_list[0] = (block_1);
+  block_list[0] = block_1;
   block_list[1] = block_2;
   block_list[2] = block_3;
   block_list[3] = block_4;
@@ -336,9 +385,13 @@ int main(){
 
 
   Game game_1;
-  for (int i=0; i<5; i++){
-    game_1.one_block(block_list[random_list[i]],map);
+  for (int a=0; a<7; a++){
+    game_1.one_block(block_list[random_list[a]],map);
+    //std::cout<<"1"<<std::endl;
     map.clear_block();
+    //std::cout<<"2"<<std::endl;
+    //block_list[random_list[a]].show_block();
+
   }
 
 
